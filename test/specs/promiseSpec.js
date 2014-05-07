4fe860f9ddf7c057d57244650c4fe0ac5048b538
Andrey Savchenko <andrey@aejis.eu>
2014-05-07 21:29:03 +0300

    Basic .map tests

--- test/specs/promiseSpec.js
+++ test/specs/promiseSpec.js
@@ -2,5 +2,6 @@ define(function(require) {
   describe('Promise', function() {
     require('specs/promiseSpec/aplus');
     require('specs/promiseSpec/allSpec');
+    require('specs/promiseSpec/mapSpec');
   });
 });
---
+++ test/specs/promiseSpec/mapSpec.js
@@ -0,0 +1,45 @@
+define(['base/promise'], function(Promise) {
+  'use strict';
+
+  var emptyArray = [],
+      valArray   = [1, 2, 3];
+
+  describe('.map', function() {
+    it('should map empty array', function(done) {
+      Promise.map(emptyArray, function(v) {
+        return v;
+      }).then(function(value) {
+        expect(value).to.be.eql(emptyArray);
+      }).then(done, expect.fail);
+    });
+
+    it('should map array of values', function(done) {
+      Promise.map(valArray, function(v) {
+        return v * 2;
+      }).then(function(values) {
+        expect(values).to.be.eql([2, 4, 6]);
+      }).then(done, expect.fail);
+    });
+
+    it('should map array of promises', function(done) {
+      var arr = [Promise.fulfilled(1), Promise.fulfilled(2), Promise.fulfilled(3)];
+
+      Promise.map(arr, function(v) {
+        return v + 2;
+      }).then(function(values) {
+        expect(values).to.be.eql([3, 4, 5]);
+      }).then(done, expect.fail);
+    });
+
+    it('should map mixed array', function(done) {
+      var arr = [1, Promise.fulfilled(2), 3];
+
+      Promise.map(arr, function() {
+        return v * 3;
+      }).then(function(values) {
+        expect(values).to.be.eql([3, 6, 9]);
+      }).then(done, expect.fail);
+    });
+  });
+
+});
